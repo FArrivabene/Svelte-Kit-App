ccdd8ce4c2d92332a713bc1898d0911891124296
Fabio Arriabene <arrivabene.fabio@outlook.com>
2023-12-14 12:11:36 +0000

    Aula 02 - Rotas

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<h1>Layout</h1>
+<ul>
+  <li><a href="/">Inicio</a></li>
+  <li><a href="/sobre">Sobre</a></li>
+  <li><a href="/portifolio">Portifólio</a></li>
+  <li><a href="/contato">Contato</a></li>
+</ul>
+
+<slot/>
