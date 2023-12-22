249ba7f07c69c3e108aee36f70dec57ebbf55a66
Fabio Arriabene <arrivabene.fabio@outlook.com>
2023-12-22 23:42:14 +0000

    Lista Ok - Falta o pesquisar

---
+++ src/routes/clicaAqui/CliqueAqui.svelte
@@ -0,0 +1,13 @@
+<script>
+  let numeroTb;
+  let labelText = "";
+
+  const numeroValue = () => {
+    const numeroValue = numeroTb.value;
+    labelText = `Número da TB: ${numeroValue}`;
+  };
+</script>
+
+<input type="text" bind:this={numeroTb} name="numero" id="numero" />
+<button on:click={numeroValue}>Pesquisar</button>
+<label>{labelText}</label>
